ea4dd94545551130def030d8afbd31655f5a10c6
eqour <eqour@yandex.ru>
2023-11-27 03:06:39 +0500

    initial commit

---
+++ src/main/java/ru/eqour/urfulibraryrest/UrfuLibraryRestApplication.java
@@ -0,0 +1,13 @@
+package ru.eqour.urfulibraryrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrfuLibraryRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrfuLibraryRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/eqour/urfulibraryrest/UrfuLibraryRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eqour.urfulibraryrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrfuLibraryRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
